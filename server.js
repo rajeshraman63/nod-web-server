a8341f86924a50424f814bb9419bbf8b862e108e
rajeshraman63 <rajeshraman63@gmail.com>
2018-06-09 21:25:21 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view_engine', 'hbs');
+
+
+// middleware
+app.use((req, res, next) => {
+	//if this function left empty ..
+	// request wiill not be called 
+
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server.log');
+		}
+	});
+
+
+	next();
+
+});
+
+
+/*
+app.use((req,res,next) => {
+	res.render('maintenance.hbs');
+});
+*/
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+
+app.get('/', (request, response) => {
+	//	response.send('<h1>Hello Express!</h1>');
+
+	/*response.send({
+		name : 'Rajesh Raman',
+		likes : ['Biking','Coding']
+	});*/
+
+	response.render('home.hbs', {
+		pageTitle: 'Home page',
+		welcomeMessage: 'Welcome to my website'
+
+	});
+
+});
+
+app.get('/about', (request, response) => {
+
+	response.render('about.hbs', {
+		pageTitle: 'About page'
+	});
+});
+
+//bad - send back jason with errorMessage
+
+app.get('/bad', (request, response) => {
+	response.send({
+		erroMessage: 'Unable to handle request'
+	});
+});
+
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
